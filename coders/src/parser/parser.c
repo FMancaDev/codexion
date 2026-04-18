5597f4ec1fe72c88b096934bba94f0ad3c7dd4fe
Fodé Manca <mancafode11@gmail.com>
2026-04-18 04:29:26 +0100

    doing memory alocation for coders and dongles, inicialization of dongle pointers

--- coders/src/parser/parser.c
+++ coders/src/parser/parser.c
@@ -14,6 +14,22 @@ static int	is_digit(char *str)
 	return (1);
 }
 
+static void	init_coders_links(t_simulation *sim)
+{
+	int	i;
+
+	i = 0;
+	while (i < sim->number_of_coders)
+	{
+		sim->coders[i].id = i + 1;
+		sim->coders[i].times_compiled = 0;
+		sim->coders[i].simulation = sim;
+		sim->coders[i].left_dongle = &sim->dongles[i];
+		sim->coders[i].right_dongle = &sim->dongles[(i + 1) % sim->number_of_coders];
+		i++;
+	}
+}
+
 int	simulation_init(t_simulation *sim, int argc, char **argv)
 {
 	int	i;
@@ -39,14 +55,30 @@ int	simulation_init(t_simulation *sim, int argc, char **argv)
 		return (1);
 	if (strcmp(argv[8], "fifo") == 0)
 		sim->scheduler_mode = FIRST_IN_FIRST_OUT;
-	else if (strcmp(argv[0], "edf") == 0)
+	else if (strcmp(argv[8], "edf") == 0)
 		sim->scheduler_mode = EARLIEST_DEADLINE_FIRST;
 	else
 		return (1);
+
+	sim->dongles = malloc(sizeof(t_dongle) * sim->number_of_coders);
+	if (!sim->dongles)
+		return (1);
+
+	sim->coders = malloc(sizeof(t_coder) * sim->number_of_coders);
+	if (!sim->coders)
+		return (free(sim->dongles), 1);
+
 	sim->simulation_should_stop = 0;
-	if (pthread_mutex_init(&sim->print_lock, NULL) != 0)
-		return (1);
-	if (pthread_mutex_init(&sim->stop_flag_lock, NULL) != 0)
-		return (1);
+	pthread_mutex_init(&sim->print_lock, NULL);
+	pthread_mutex_init(&sim->stop_flag_lock, NULL);
+	
+	i = 0;
+	while (i < sim->number_of_coders)
+	{
+		if (dongle_init(&sim->dongles[i], sim->number_of_coders) != 0)
+			return (1);
+		i++;
+	}
+	init_coders_links(sim);
 	return (0);
 }
