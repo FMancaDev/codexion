4c10268bbf387476adb3bb2cf9f004edd05d8f74
Fodé Manca <mancafode11@gmail.com>
2026-04-22 01:36:22 +0100

    doing dongle decision with priority queuing and cooldown

--- coders/src/dongle/dongle.c
+++ coders/src/dongle/dongle.c
@@ -4,7 +4,7 @@ void	dongle_destroy(t_dongle *dongle)
 {
 	pthread_mutex_destroy(&dongle->access_lock);
 	pthread_cond_destroy(&dongle->became_available);
-	pqueue_free(&dongle->waiting_coders); // liberta memoria da heap
+	pqueue_free(&dongle->waiting_coders);
 }
 
 int	dongle_init(t_dongle *dongle, int max_waiting_coders)
@@ -16,10 +16,8 @@ int	dongle_init(t_dongle *dongle, int max_waiting_coders)
 		pthread_mutex_destroy(&dongle->access_lock);
 		return (1);
 	}
-
 	dongle->is_held = 0;
 	dongle->available_after_ms = 0;
-
 	if (pqueue_init(&dongle->waiting_coders, max_waiting_coders) != 0)
 	{
 		pthread_mutex_destroy(&dongle->access_lock);
@@ -27,4 +25,55 @@ int	dongle_init(t_dongle *dongle, int max_waiting_coders)
 		return (1);
 	}
 	return (0);
+}
+
+void	dongle_pick_up(t_dongle *dongle, t_coder *coder)
+{
+	long long	key;
+
+	pthread_mutex_lock(&dongle->access_lock);
+	
+	if (coder->simulation->scheduler_mode == FIRST_IN_FIRST_OUT)
+		key = get_time_ms();
+	else
+		key = coder->last_compile_started_at + coder->simulation->time_to_burnout_ms;
+
+	pqueue_push(&dongle->waiting_coders, coder->id, key);
+
+	while (dongle->is_held 
+		|| get_time_ms() < dongle->available_after_ms 
+		|| dongle->waiting_coders.entries[0].coder_id != coder->id)
+	{
+		pthread_mutex_lock(&coder->simulation->stop_flag_lock);
+		if (coder->simulation->simulation_should_stop)
+		{
+			pthread_mutex_unlock(&coder->simulation->stop_flag_lock);
+			break ;
+		}
+		pthread_mutex_unlock(&coder->simulation->stop_flag_lock);
+
+		pthread_cond_wait(&dongle->became_available, &dongle->access_lock);
+	}
+
+	pthread_mutex_lock(&coder->simulation->stop_flag_lock);
+	if (!coder->simulation->simulation_should_stop)
+	{
+		pqueue_pop(&dongle->waiting_coders);
+		dongle->is_held = 1;
+	}
+	pthread_mutex_unlock(&coder->simulation->stop_flag_lock);
+	
+	pthread_mutex_unlock(&dongle->access_lock);
+}
+
+void	dongle_put_down(t_dongle *dongle, t_simulation *sim)
+{
+	pthread_mutex_lock(&dongle->access_lock);
+	
+	dongle->is_held = 0;
+	dongle->available_after_ms = get_time_ms() + sim->dongle_cooldown_ms;
+	
+	pthread_cond_broadcast(&dongle->became_available);
+	
+	pthread_mutex_unlock(&dongle->access_lock);
 }
