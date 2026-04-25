d994bf9e3565eef5ed24388ba5941d4c9efc4665
Fodé Manca <mancafode11@gmail.com>
2026-04-25 19:32:29 +0100

    final fix's

--- coders/src/simulation/monitor.c
+++ coders/src/simulation/monitor.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 19:20:12 by fomanca           #+#    #+#             */
+/*   Updated: 2026/04/25 19:31:10 by fomanca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/codexion.h"
 
 static int	check_individual_burnout(t_simulation *sim, t_coder *coder)
