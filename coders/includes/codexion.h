8e131920b232841ce7cb883fbce5a7adf1f342d7
Fode Manca <fomanca@c2r6s11.42porto.com>
2026-07-19 23:33:06 +0100

    fixing norminette bugs

--- coders/includes/codexion.h
+++ coders/includes/codexion.h
@@ -6,7 +6,7 @@
 /*   By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 19:26:23 by fomanca           #+#    #+#             */
-/*   Updated: 2026/07/19 17:17:32 by fomanca          ###   ########.fr       */
+/*   Updated: 2026/07/19 18:50:51 by fomanca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,24 @@ typedef struct s_waiting_coder
 	int			coder_id;
 	long long	priority_key;
 	long long	sequence;
-}t_waiting_coder;
+}	t_waiting_coder;
 
 typedef struct s_priority_queue
 {
 	t_waiting_coder	*entries;
 	int				count;
 	int				capacity;
-}t_priority_queue;
+}	t_priority_queue;
 
 typedef struct s_dongle
 {
-	pthread_mutex_t	lock;
-	pthread_cond_t	available;
-	int				held;
-	long long		available_after_ms;
-	long long		next_sequence;
+	pthread_mutex_t		lock;
+	pthread_cond_t		available;
+	int					held;
+	long long			available_after_ms;
+	long long			next_sequence;
 	t_priority_queue	queue;
-}t_dongle;
+}	t_dongle;
 
 typedef struct s_coder
 {
@@ -60,7 +60,7 @@ typedef struct s_coder
 	t_dongle		*left;
 	t_dongle		*right;
 	t_simulation	*sim;
-}t_coder;
+}	t_coder;
 
 typedef struct s_simulation
 {
@@ -87,46 +87,45 @@ typedef struct s_simulation
 	int				base_mutexes_initialized;
 	t_dongle		*dongles;
 	t_coder			*coders;
-}t_simulation;
+}	t_simulation;
 
 int			parse_arguments(t_simulation *sim, int argc, char **argv);
 int			parse_positive(const char *str, long long *value, int allow_zero);
 int			simulation_init(t_simulation *sim, int argc, char **argv);
 int			simulation_run(t_simulation *sim);
-void			simulation_destroy(t_simulation *sim);
+void		simulation_destroy(t_simulation *sim);
 int			simulation_stopped(t_simulation *sim);
 int			simulation_stop(t_simulation *sim);
-void			wake_all_dongles(t_simulation *sim);
-void			wait_for_start(t_simulation *sim);
+void		wake_all_dongles(t_simulation *sim);
+void		wait_for_start(t_simulation *sim);
 
 long long	get_time_ms(void);
 long long	time_since_start(t_simulation *sim);
 int			smart_sleep(t_simulation *sim, long long duration_ms);
-void			make_timeout(struct timespec *ts, long long wait_ms);
+void		make_timeout(struct timespec *ts, long long wait_ms);
 
-void			print_status(t_coder *coder, const char *message);
-void			print_burnout(t_coder *coder);
+void		print_status(t_coder *coder, const char *message);
+void		print_burnout(t_coder *coder);
 
 int			pqueue_init(t_priority_queue *pq, int capacity);
-void			pqueue_free(t_priority_queue *pq);
+void		pqueue_free(t_priority_queue *pq);
 int			pqueue_push(t_priority_queue *pq, t_waiting_coder entry);
 int			pqueue_pop(t_priority_queue *pq, t_waiting_coder *entry);
 int			pqueue_remove(t_priority_queue *pq, int coder_id);
 int			pqueue_is_empty(t_priority_queue *pq);
 int			waiter_before(t_waiting_coder a, t_waiting_coder b);
-void			waiter_swap(t_waiting_coder *a, t_waiting_coder *b);
+void		waiter_swap(t_waiting_coder *a, t_waiting_coder *b);
 
 int			dongle_init(t_dongle *dongle, int capacity);
-void			dongle_destroy(t_dongle *dongle);
+void		dongle_destroy(t_dongle *dongle);
 int			dongle_pick_up(t_dongle *dongle, t_coder *coder);
-void			dongle_put_down(t_dongle *dongle, t_simulation *sim);
+void		dongle_put_down(t_dongle *dongle, t_simulation *sim);
 
-void			*coder_life_cycle(void *arg);
-void			*burnout_monitor(void *arg);
+void		*coder_life_cycle(void *arg);
+void		*burnout_monitor(void *arg);
 long long	coder_last_compile(t_coder *coder);
 int			coder_compile_count(t_coder *coder);
-void			coder_mark_compile_start(t_coder *coder);
-void			coder_increment_compile_count(t_coder *coder);
+void		coder_mark_compile_start(t_coder *coder);
+void		coder_increment_compile_count(t_coder *coder);
 
 #endif
-
